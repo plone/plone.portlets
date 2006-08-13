2f1aeaeeb169164bb52b7eb7ad729129f289c292
Geir Baekholt <geir@baekholt.com>
2006-08-13 20:45:03 +0000

    clean up stray whitespace
    svn path=/plone.portlets/trunk/; revision=10425

--- plone/portlets/manager.py
+++ plone/portlets/manager.py
@@ -13,15 +13,15 @@ from plone.portlets.storage import PortletStorage
 
 class PortletManagerRenderer(object):
     """Default renderer for portlet managers.
-    
+
     When the zope.contentprovider handler for the provider: expression looks up
-    a name, context, it will find an adapter factory that creates an instance 
+    a name, context, it will find an adapter factory that creates an instance
     of this class. In fact, this will be a site-local registration, and the
-    actual implementation of the adapter factory will be the (persistent) 
+    actual implementation of the adapter factory will be the (persistent)
     IPortletManager, which has a suitable __call__() method.
     """
     implements(IPortletManagerRenderer)
-    
+
     def __init__(self, manager, context, request, view):
         self.__parent__ = view
         self.manager = manager
@@ -29,22 +29,22 @@ class PortletManagerRenderer(object):
         self.request = request
         self.template = None
         self.__updated = False
-        
+
     def filter(self, portlets):
         return portlets
-        
+
     def update(self):
         self.__updated = True
 
         portletContext = IPortletContext(self.context)
         retriever = getMultiAdapter((portletContext, self.manager), IPortletRetriever)
-                
+
         self.portlets = [self._dataToPortlet(a.data)
                             for a in self.filter(retriever.getPortlets())]
-    
+
         for p in self.portlets:
             p.update()
-            
+
     def render(self):
         if not self.__updated:
             raise UpdateNotCalled()
@@ -52,27 +52,27 @@ class PortletManagerRenderer(object):
             return self.template(portlets=self.portlets)
         else:
             return u'\n'.join([p.render() for p in self.portlets])
-        
+
     def _dataToPortlet(self, data):
         """Helper method to get the correct IPortletRenderer for the given
         data object.
         """
-        return getMultiAdapter((self.context, self.request, self.__parent__, 
+        return getMultiAdapter((self.context, self.request, self.__parent__,
                                     self.manager, data,), IPortletRenderer)
-        
-    
+
+
 class PortletManager(PortletStorage):
     """Default implementation of the portlet manager.
-    
+
     Provides the functionality that allows the portlet manager to act as an
     adapter factory.
     """
-    
+
     implements(IPortletManager)
 
     __name__ = __parent__ = None
 
     def __call__(self, context, request, view):
         return PortletManagerRenderer(self, context, request, view)
-        
-                                    
+
+
