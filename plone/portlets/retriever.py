78d3681d2271f3d7a6f008e7c4f868d11d272c66
Geir Baekholt <geir@baekholt.com>
2006-08-13 20:45:45 +0000

    clean up whitespace
    svn path=/plone.portlets/trunk/; revision=10426

--- plone/portlets/retriever.py
+++ plone/portlets/retriever.py
@@ -7,33 +7,33 @@ from plone.portlets.interfaces import IPortletRetriever
 
 class DefaultPortletRetriever(object):
     """A volatile default portlet storage.
-    
+
     This will most likely need to be override to become persistent.
     """
-    
+
     implements(IPortletRetriever)
     adapts(IPortletContext, IPortletStorage)
-    
+
     def __init__(self, context, storage):
         self.context = context
         self.storage = storage
-        
+
     def getPortlet(self, id):
         portlets = [p for p in self.getPortlets() if p.id == id]
         if len(portlets) == 0:
             raise KeyError
         else:
             return portlets[0]
-        
+
     def getPortlets(self):
         # Get user portlets
         userPortlets = self.storage.getPortletAssignmentsForUser(self.context.userId)
-        
+
         # Get group portlets and combine them into a single list
         groupPortlets = []
         for g in self.context.groupIds:
             groupPortlets.extend(self.storage.getPortletAssignmentsForGroup(g))
-        
+
         # Get context portlets, including those for parents
         contextPortlets = []
         location = self.context
@@ -41,5 +41,5 @@ class DefaultPortletRetriever(object):
             parentContext = IPortletContext(location)
             contextPortlets.extend(self.storage.getPortletAssignmentsForContext(location.uid))
             location = location.parent
-        
+
         return contextPortlets + userPortlets + groupPortlets
