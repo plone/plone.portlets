aafd94bc41f0fcd7547cdb406df01164fb118112
Martin Aspeli <optilude@gmail.com>
2006-08-20 00:12:38 +0000

    Explicitly load dependent packages in configure.zcml instead of tests, since they may not be loaded when Zope is loaded.
    svn path=/plone.portlets/trunk/; revision=10526

--- plone/portlets/tests.py
+++ plone/portlets/tests.py
@@ -1,10 +1,5 @@
 import unittest
 
-import zope.app.component
-import zope.app.pagetemplate
-import zope.security
-import zope.app.security
-import zope.contentprovider
 import plone.portlets
 
 from zope.testing import doctest
@@ -15,14 +10,6 @@ optionflags = doctest.NORMALIZE_WHITESPACE | doctest.ELLIPSIS
 
 def configurationSetUp(test):
     setUp()
-    
-    XMLConfig('meta.zcml', zope.security)()
-    XMLConfig('meta.zcml', zope.app.security)()
-    XMLConfig('meta.zcml', zope.app.component)()
-    XMLConfig('meta.zcml', zope.app.pagetemplate)()
-    
-    XMLConfig('configure.zcml', zope.app.security)()
-    XMLConfig('configure.zcml', zope.contentprovider)()
     XMLConfig('configure.zcml', plone.portlets)()
 
 
