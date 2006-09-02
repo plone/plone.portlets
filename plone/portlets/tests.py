93e88b9b06c01040f48aa8dcd65a1b92638390b6
Martin Aspeli <optilude@gmail.com>
2006-09-02 22:01:39 +0000

    Fix tests by doing set-up there instead of in configure.zcml
    svn path=/plone.portlets/trunk/; revision=10763

--- plone/portlets/tests.py
+++ plone/portlets/tests.py
@@ -2,6 +2,15 @@ import unittest
 
 import plone.portlets
 
+import zope.security
+import zope.app.security
+import zope.app.component
+import zope.app.pagetemplate
+
+import zope.app.event
+import zope.app.container
+import zope.contentprovider
+
 from zope.testing import doctest
 from zope.component.testing import setUp, tearDown
 from zope.configuration.xmlconfig import XMLConfig
@@ -10,6 +19,17 @@ optionflags = doctest.NORMALIZE_WHITESPACE | doctest.ELLIPSIS
 
 def configurationSetUp(test):
     setUp()
+    
+    XMLConfig('meta.zcml', zope.security)()
+    XMLConfig('meta.zcml', zope.app.security)()
+    XMLConfig('meta.zcml', zope.app.component)()
+    XMLConfig('meta.zcml', zope.app.pagetemplate)()
+    
+    XMLConfig('configure.zcml', zope.app.security)()
+    XMLConfig('configure.zcml', zope.app.event)()
+    XMLConfig('configure.zcml', zope.app.container)()
+    XMLConfig('configure.zcml', zope.contentprovider)()
+    
     XMLConfig('configure.zcml', plone.portlets)()
 
 
