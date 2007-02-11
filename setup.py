3c377c8edd30d2dad6ee1e51061bd84fea07b2a2
Martin Aspeli <optilude@gmail.com>
2007-02-11 03:18:47 +0000

    Updating setup.py
    svn path=/plone.portlets/trunk/; revision=12304

--- setup.py
+++ setup.py
@@ -7,10 +7,15 @@ setup(name='plone.portlets',
       version=version,
       description="An extension of zope.viewlet to support dynamic portlets",
       long_description="""\
+plone.portlets provides a generic infrastructure for managing portlets. 
+Portlets are a bit like viewlets, except they can be manipulated at runtime,
+using local components. This package is used by plone.app.portlets to provide
+Plone-specific portlets, but should be generic enough to work on other 
+platforms. It should work in a "pure Zope 3" environment.
 """,
       classifiers=[], # Get strings from http://www.python.org/pypi?%3Aaction=list_classifiers
       keywords='portlet viewlet',
-      author='Plone Foundation',
+      author='Martin Aspeli',
       author_email='plone-developers@lists.sourceforge.net',
       url='http://svn.plone.org/svn/plone/plone.portlets',
       license='GPL',
