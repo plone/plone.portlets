aeab13bf2d78ae32c74b2ece8906217f1f447558
George Lee <veggienet@gmail.com>
2007-12-31 06:36:02 +0000

    Added forgotten variable!
    svn path=/plone.portlets/branches/plip205-portlettypes-multiplemanagers/; revision=18706

--- plone/portlets/manager.py
+++ plone/portlets/manager.py
@@ -145,7 +145,7 @@ class PortletManager(PortletStorage):
            #BBB - first condition, because starting with Plone 3.1
            #every p[1].for_ should be a list
            if type(p[1].for_) not in (tuple, list): 
-               log_deprecated("Portlet type %s is using a " % addview + \
+               log_deprecated("Portlet type %s is using a " % p[1].addview + \
                  "deprecated format for storing interfaces of portlet " \
                  "managers where it is addable. Its for_ attribute should " \
                  "be a list of portlet manager interfaces, using [] for the " \
