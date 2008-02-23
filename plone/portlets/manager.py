5008f7240a6f15cd7375727712d1250b59f98eef
George Lee <veggienet@gmail.com>
2008-02-23 20:27:31 +0000

    Merge PLIPs 205, 207, and 218
    svn path=/plone.portlets/trunk/; revision=19475

--- plone/portlets/manager.py
+++ plone/portlets/manager.py
@@ -138,5 +138,20 @@ class PortletManager(PortletStorage):
         return getMultiAdapter((context, request, view, self), IPortletManagerRenderer)
 
     def getAddablePortletTypes(self):
-        return [p[1] for p in getUtilitiesFor(IPortletType) 
-                    if p[1].for_ is None or p[1].for_.providedBy(self)]        
+       addable = []
+       for p in getUtilitiesFor(IPortletType):
+           #BBB - first condition, because starting with Plone 3.1
+           #every p[1].for_ should be a list
+           if type(p[1].for_) is not list:
+               logger.warning("Deprecation Warning Portlet type " + \
+                 "%s is using a deprecated format for " % p[1].addview + \
+                 "storing interfaces of portlet managers where it is " + \
+                 "addable. Its for_ attribute should be a list of portlet " + \
+                 "manager interfaces, using [zope.interface.Interface] " + \
+                 "for the portlet type to be addable anywhere. The old " + \
+                 "format will be unsupported in Plone 4.0.")
+               if p[1].for_ is None or p[1].for_.providedBy(self):
+                   addable.append(p[1])
+           elif [i for i in p[1].for_ if i.providedBy(self)]:
+               addable.append(p[1])
+       return addable
