1c8d722ccacdd85cbc65229b5d6b854c0b80f65f
Hanno Schlichting <hanno@hannosch.eu>
2009-01-09 13:04:39 +0000

    Specify package dependencies
    svn path=/plone.portlets/trunk/; revision=24137

--- setup.py
+++ setup.py
@@ -13,21 +13,33 @@ using local components. This package is used by plone.app.portlets to provide
 Plone-specific portlets, but should be generic enough to work on other 
 platforms. It should work in a "pure Zope 3" environment.
 """,
-      classifiers=[], # Get strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[],
       keywords='portlet viewlet',
       author='Martin Aspeli',
       author_email='plone-developers@lists.sourceforge.net',
       url='http://svn.plone.org/svn/plone/plone.portlets',
-      license='LGPL',
+      license='GPL',
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['plone'],
       include_package_data=True,
       zip_safe=False,
       install_requires=[
         'setuptools',
+        'ZODB3',
         'plone.memoize',
+        'zope.annotation',
+        'zope.component',
+        'zope.configuration',
+        'zope.contentprovider',
+        'zope.interface',
+        'zope.publisher',
+        'zope.schema',
+        'zope.security',
+        'zope.app.component',
+        'zope.app.container',
+        'zope.app.content',
+        'zope.app.folder',
+        'zope.app.pagetemplate',
+        'zope.app.security',
       ],
-      entry_points="""
-      # -*- Entry points: -*-
-      """,
       )
